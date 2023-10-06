7c4d8de37500adf2862844e08d537ab508ce9a82
PatzminiHD <7.0@gmx.at>
2023-10-06 12:03:57 +0200

    Add new Project

---
+++ 5AHME/CSharp/01_Linear_Interpolation/Program.cs
@@ -0,0 +1,81 @@
+﻿//*********************************************************************************************
+//  Project:    _01_Linear_Interpolation
+//  Namespace:  _01_Linear_Interpolation
+//  Path:       E:\School\AIIT\5AHME\CSharp\
+//  File:       Program.cs
+// --------------------------------------------------------------------------------------------
+//  Date:       10/6/2023 12:02:01 PM
+//  Username:	PatzminiHD
+//  Developer:  <PatzminiHD>
+// --------------------------------------------------------------------------------------------
+//  Machine:	DESKTOP-UIJDDHM
+//  System-OS:  WIN11-Enterprise
+//  IDE:        MS Visual Studio Community 2022
+// --------------------------------------------------------------------------------------------
+//  
+// --------------------------------------------------------------------------------------------
+//  Description of Task:
+//	
+//              				
+//				
+// 
+// --------------------------------------------------------------------------------------------
+//  Date:                       Author:                     Reason for Change
+//  10/6/2023 12:02:01 PM       <PatzminiHD>                Creation of project ....
+//*********************************************************************************************
+
+// ----------------------------- using Directives to include different namespaces -------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//  -------------------------------------------- START -----------------------------------------
+
+namespace _01_Linear_Interpolation
+{
+    internal class Program
+    {
+        // --------------------------------------------------------
+        //	Hauptprogramm:   Main()
+        // --------------------------------------------------------		
+
+        static void Main(string[] args)
+        {
+
+            //	-----------------------------------------------
+            //	Setups
+            //	-----------------------------------------------
+            Console.Title = "_01_Linear_Interpolation";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //	-----------------------------------------------
+            //	Definition of variables & data structures
+            //	-----------------------------------------------
+
+
+
+
+            //	-----------------------------------------------
+            //	Instructions & Procedures
+            //	-----------------------------------------------
+
+
+
+            //	-----------------------------------------------
+            //	Programm-Ende
+            //	-----------------------------------------------
+            Console.WriteLine("\n\n\tENTER => Programm-Ende\n\n");
+            Console.Read();
+
+        }   // --> end of main	
+
+
+    }   // --> end of class
+
+
+}   //  --> end of namespace
+
+//  -------------------------------------------- END  -----------------------------------------
