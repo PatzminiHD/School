ba7aa8c96d7ef79b708287f071a247dc5def42e1
PatzminiHD <7.0@gmx.at>
2024-03-08 11:59:12 +0100

    Use Library

---
+++ 5AHME/CSharp/240209_Use_Lib_Vererbung/Program.cs
@@ -0,0 +1,23 @@
+﻿using _240126_Lib_Inherit;
+
+namespace _240209_Use_Lib_Vererbung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Interpolate interpolate = new Interpolate()
+            {
+                X1 = 1,
+                X2 = 3,
+                Y1 = 1,
+                Y2 = 3,
+                X = 2,
+            };
+
+            Console.WriteLine($"Slope:     {interpolate.CalcSlope()}");
+            Console.WriteLine($"Intersect: {interpolate.intercept()}");
+            Console.WriteLine($"Y:         {interpolate.Y()}");
+        }
+    }
+}
